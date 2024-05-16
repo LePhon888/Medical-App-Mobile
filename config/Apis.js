39d0d9ff73e3da8fbd7ba16fb2c6e8f6268ea2a6
Le Phon <lephon888@gmail.com>
2024-05-16 11:54:47 +0700

    fix sign up

--- config/Apis.js
+++ config/Apis.js
@@ -7,7 +7,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // export const SERVER = "http://192.168.1.8:8080";
 const CHATBOT = "http://34.125.230.219:8000";
-export const SERVER = "http://34.125.230.219:8080";
+export const SERVER = "http://34.66.33.6:8080";
 //const SERVER = "http://medicalapp.com";
 
 export const endpoints = {
